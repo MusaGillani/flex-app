cc8434c386c1874041b9e97ca1ec6c3ee00720bf
musa gillani <musagillani1@gmail.com>
2021-11-21 20:00:07 +0500

    ingredient adding UI text fields complete

--- lib/screens/home_pages/restaurants.dart
+++ lib/screens/home_pages/restaurants.dart
@@ -9,13 +9,42 @@ class Restaurants extends StatefulWidget {
 
 class _RestaurantsState extends State<Restaurants> {
   final GlobalKey<FormState> _formKey = GlobalKey(debugLabel: 'resform-key');
-  // List<String> _itemTypes = <String>[
-  //   'Fruits',
-  //   'Vegetables',
-  //   'Nuts and Seeds',
-  //   'Dairy'
-  // ];
-  String? _dropDownValue = 'Fruits';
+  // String? _dropDownValue = 'Fruits';
+
+  List<Widget> _ingredientEntry = [];
+  List<String> _ingredients = [];
+
+  var init = true;
+  late final Size deviceSize;
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (init) {
+      deviceSize = MediaQuery.of(context).size;
+      // _ingredients.add('');
+      _ingredientEntry.add(
+        _buildTextFormField(
+          BoxConstraints(
+            maxHeight: deviceSize.height,
+            maxWidth: deviceSize.width,
+          ),
+          hintText: 'ingredient name',
+          validator: (String? val) {
+            if (val!.isEmpty) {
+              return 'required!';
+            }
+            return null;
+          },
+          onSaved: (String? val) {
+            _ingredients.add(val!);
+          },
+          keyboard: TextInputType.name,
+        ),
+      );
+    }
+    init = false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -35,6 +64,12 @@ class _RestaurantsState extends State<Restaurants> {
             style: TextStyle(color: Colors.grey.shade400, fontSize: 15),
           ),
           centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.check),
+              onPressed: _submit,
+            ),
+          ],
         ),
         body: LayoutBuilder(
           builder: (
@@ -49,6 +84,7 @@ class _RestaurantsState extends State<Restaurants> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  SizedBox(height: 10),
                   _buildTextFormField(
                     constraints,
                     hintText: 'meal name',
@@ -76,7 +112,101 @@ class _RestaurantsState extends State<Restaurants> {
                   ),
                   SizedBox(height: 10),
                   Text('Enter Ingredients: '),
-                  ListTile(
+                  Expanded(
+                    child: ListView.separated(
+                      itemCount: _ingredientEntry.length,
+                      itemBuilder: (_, i) => _ingredientEntry[i],
+                      separatorBuilder: (_, i) => SizedBox(height: 10),
+                    ),
+                  ),
+                  // ..._ingredientEntry,
+                ],
+              ),
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: _addField,
+          label: const Text('Add Ingredient'),
+          icon: const Icon(Icons.add),
+          backgroundColor: Colors.pink.shade50,
+        ),
+      ),
+    );
+  }
+
+  void _submit() {
+    if (!_formKey.currentState!.validate()) return;
+    _formKey.currentState!.save();
+    // for (var item in _ingredients) {
+    //   print(item);
+    // }
+    // setState(() {
+
+    // });
+  }
+
+  void _addField() {
+    setState(() {
+      _ingredientEntry.add(
+        _buildTextFormField(
+          BoxConstraints(
+            maxHeight: deviceSize.height,
+            maxWidth: deviceSize.width,
+          ),
+          hintText: 'ingredient name',
+          validator: (String? val) {
+            if (val!.isEmpty) {
+              return 'required!';
+            }
+            return null;
+          },
+          onSaved: (String? val) {
+            _ingredients.add(val!);
+          },
+          keyboard: TextInputType.name,
+        ),
+      );
+    });
+  }
+
+  // Widget _buildIngredientSelector() {
+  //   return ListTile(
+  //     title: _buildTextFormField(constraints, hintText: hintText, validator: validator, onSaved: onSaved, keyboard: keyboard),
+  //   );
+  // }
+
+  Widget _buildTextFormField(
+    BoxConstraints constraints, {
+    required String hintText,
+    required String? Function(String? val) validator,
+    required void Function(String? val) onSaved,
+    required TextInputType keyboard,
+  }) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: constraints.maxWidth * 0.1,
+      ),
+      child: TextFormField(
+        keyboardType: keyboard,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.grey.shade300,
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          hintText: hintText,
+        ),
+        validator: validator,
+        onSaved: onSaved,
+      ),
+    );
+  }
+}
+
+/*
+ListTile(
                     title: _buildTextFormField(
                       constraints,
                       hintText: 'ingredient name',
@@ -111,46 +241,4 @@ class _RestaurantsState extends State<Restaurants> {
                           .toList(),
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  // Widget _buildIngredientSelector() {
-  //   return ListTile(
-  //     title: _buildTextFormField(constraints, hintText: hintText, validator: validator, onSaved: onSaved, keyboard: keyboard),
-  //   );
-  // }
-
-  Padding _buildTextFormField(
-    BoxConstraints constraints, {
-    required String hintText,
-    required String? Function(String? val) validator,
-    required void Function(String? val) onSaved,
-    required TextInputType keyboard,
-  }) {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: constraints.maxWidth * 0.1,
-      ),
-      child: TextFormField(
-        keyboardType: keyboard,
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: Colors.grey.shade300,
-          border: OutlineInputBorder(
-            borderSide: BorderSide.none,
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          hintText: hintText,
-        ),
-        validator: validator,
-        onSaved: onSaved,
-      ),
-    );
-  }
-}
+ */
